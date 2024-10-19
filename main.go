f7e1d2341f9719968342afa6befedccfff8ad6b0
RaptorExTolop <jamesappletonret@gmail.com>
2024-10-19 15:59:35 +1030

    2: added background music from animal crossing for this demo

--- main.go
+++ main.go
@@ -8,16 +8,20 @@ const (
 )
 
 var (
-	running   = true
-	bkgcolour = rl.NewColor(147, 211, 196, 255)
+	// uncategorised
+	running     = true
+	bkgcolour   = rl.NewColor(147, 211, 196, 255)
+	grassSprite rl.Texture2D
 
-	grassSprite  rl.Texture2D
+	// player vars
 	playerSprite rl.Texture2D
+	playerSrc    rl.Rectangle
+	playerDest   rl.Rectangle
+	playerSpeed  float32 = 3
 
-	playerSrc  rl.Rectangle
-	playerDest rl.Rectangle
-
-	playerSpeed float32 = 3
+	// music vars
+	musicPaused bool
+	music       rl.Music
 )
 
 func drawScene() {
@@ -26,6 +30,7 @@ func drawScene() {
 }
 
 func input() {
+	// movement
 	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
 		playerDest.Y -= playerSpeed
 	}
@@ -37,6 +42,10 @@ func input() {
 	}
 	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
 		playerDest.X += playerSpeed
+	}
+	// debugging
+	if rl.IsKeyPressed(rl.KeyQ) {
+		musicPaused = !musicPaused
 	}
 }
 
@@ -50,7 +59,16 @@ func render() {
 }
 
 func update() {
+	// should close window
 	running = !rl.WindowShouldClose()
+
+	// bkg music
+	rl.UpdateMusicStream(music) // update the music sound
+	if musicPaused {
+		rl.PauseMusicStream(music)
+	} else {
+		rl.ResumeMusicStream(music)
+	} // pauses music based of off input
 }
 
 func init() {
@@ -58,16 +76,30 @@ func init() {
 	rl.SetExitKey(0)
 	rl.SetTargetFPS(60)
 
+	// texture/sprite loading
 	grassSprite = rl.LoadTexture("sproutLandsPack/Tilesets/Grass.png")
 	playerSprite = rl.LoadTexture("sproutLandsPack/Characters/BasicCharakterSpritesheet.png")
 
+	// player Src and Dest
 	playerSrc = rl.NewRectangle(0, 0, 48, 48)
 	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
+	// music
+	rl.InitAudioDevice()
+	music = rl.LoadMusicStream("sproutLandsPack/Music/AnimalCrossingCampMusicMorning.mp3") // sets musics file path
+	musicPaused = false                                                                    // makes sure musicPaused if false so music starts playing
+	rl.PlayMusicStream(music)
+	rl.SetMusicVolume(music, 0.1) // sets volume for music
 }
 
 func quit() {
+	// unload textures
 	rl.UnloadTexture(grassSprite)
 	rl.UnloadTexture(playerSprite)
+	rl.UnloadMusicStream(music)
+	rl.CloseAudioDevice()
+
+	// close window
 	rl.CloseWindow()
 }
 
