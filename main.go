fb6f10f810a37e4b960e75f2f78d97c88efd6d2e
RaptorExTolop <jamesappletonret@gmail.com>
2024-10-19 14:20:04 +1030

    0.5 added sproutlands pack and started to setup functions in main

--- main.go
+++ main.go
@@ -2,18 +2,38 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+const (
+	screenWidth  = 1280
+	screenheight = 720
+)
+
+var ()
+
+func drawScene() {}
+
+func input() {}
+
+func render() {
+	rl.BeginDrawing()
+
+	rl.ClearBackground(rl.RayWhite)
+
+	drawScene()
+
+	rl.EndDrawing()
+}
+
+func update() {}
+
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
-	defer rl.CloseWindow()
+	rl.InitWindow(screenWidth, screenheight, "raylib [core] example - basic window")
 
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
-		rl.EndDrawing()
+		input()
+		update()
+		render()
 	}
+	rl.CloseWindow()
 }
