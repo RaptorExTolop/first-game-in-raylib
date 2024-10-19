8149d365cacf39456c9abadee23d2067dc3d3e00
RaptorExTolop <jamesappletonret@gmail.com>
2024-10-19 15:32:43 +1030

    1: finished setup of main.go & added basic player & player controller

--- main.go
+++ main.go
@@ -10,11 +10,35 @@ const (
 var (
 	running   = true
 	bkgcolour = rl.NewColor(147, 211, 196, 255)
+
+	grassSprite  rl.Texture2D
+	playerSprite rl.Texture2D
+
+	playerSrc  rl.Rectangle
+	playerDest rl.Rectangle
+
+	playerSpeed float32 = 3
 )
 
-func drawScene() {}
+func drawScene() {
+	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+}
 
-func input() {}
+func input() {
+	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
+		playerDest.Y -= playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
+		playerDest.Y += playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
+		playerDest.X -= playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
+		playerDest.X += playerSpeed
+	}
+}
 
 func render() {
 	rl.BeginDrawing()
@@ -33,9 +57,17 @@ func init() {
 	rl.InitWindow(screenWidth, screenheight, "first game using raylib & go")
 	rl.SetExitKey(0)
 	rl.SetTargetFPS(60)
+
+	grassSprite = rl.LoadTexture("sproutLandsPack/Tilesets/Grass.png")
+	playerSprite = rl.LoadTexture("sproutLandsPack/Characters/BasicCharakterSpritesheet.png")
+
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
 }
 
 func quit() {
+	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(playerSprite)
 	rl.CloseWindow()
 }
 
